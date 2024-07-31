95afadb033d16eb71ab8500f1211d6ad291a275c
Ruslanqaa <ruskiseljov1@gmail.com>
2024-07-31 23:29:19 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+my_dict = {'Ruslan': 1994, 'Maria': 1999, 'Eva': 2022}
+print(my_dict)
+print(my_dict['Ruslan'])
+print(my_dict.get('Andrey'))
+my_dict.update({'Monica': 2015, 'Leila': 2018})
+a = my_dict.pop('Leila')
+print(a)
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 5, 1, 2, 3, True, 'String', (1, 2, 3)}
+print(my_set)
+my_set.update({8,15})
+my_set.discard(5)
+print(my_set)
+
+
+
+
+
